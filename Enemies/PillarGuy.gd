0c4f68465dc77533a7f5b5018022282a71ecc162
GregCK <3bluejay@gmail.com>
2021-06-29 16:27:26 -0400

    death effect

--- Enemies/PillarGuy.gd
+++ Enemies/PillarGuy.gd
@@ -1,9 +1,14 @@
 extends KinematicBody2D
 
+const EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
 
 onready var blinkAnimationPlayer = $BlinkAnimationPlayer
 onready var stats = $Stats
 
+var parent = null
+
+func _ready():
+	parent = get_parent()
 
 
 func _on_Hurtbox_area_entered(area):
@@ -13,4 +18,9 @@ func _on_Hurtbox_area_entered(area):
 
 
 func _on_Stats_no_health():
+	var effect = EnemyDeathEffect.instance()
+	effect.set_global_position(get_global_position())
+	parent.add_child(effect)
+	
 	queue_free()
+	
