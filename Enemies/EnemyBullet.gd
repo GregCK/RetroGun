dfe5c80941c52e6f3e4169fac6990f98fca7652f
GregCK <3bluejay@gmail.com>
2021-07-01 00:47:40 -0400

    goblin spawn orbs

--- Enemies/EnemyBullet.gd
+++ Enemies/EnemyBullet.gd
@@ -3,7 +3,7 @@ extends KinematicBody2D
 
 var direction : Vector2
 var velocity : Vector2
-const speed = 50
+const speed = 100
 
 
 # Called when the node enters the scene tree for the first time.
--- Enemies/Goblin.gd
+++ Enemies/Goblin.gd
@@ -4,14 +4,13 @@ signal state_changed(new_state)
 
 const Bullet = preload("res://Enemies/EnemyBullet.tscn")
 
-
-onready var playerDetectionZone = $PlayerDetectionZone
+onready var sprite = $Sprite
 onready var animationPlayer = $AnimationPlayer
 onready var firePoint = $FirePoint
 onready var shotTimer = $ShotTimer
 onready var playerCast = $FirePoint/PlayerCast
 
-export var shots_to_fire = 4
+export var shots_to_fire = 3
 var shots_fired = 0
 
 enum State{
@@ -31,29 +30,34 @@ func _ready():
 
 
 func _physics_process(delta):
-	if player != null:
-		can_see_player()
 	
 	match current_state:
 		State.PATROL:
-			pass
+			if can_see_player():
+				set_state(State.AIM)
 		State.AIM:
 			pass
 		State.SHOOT:
 			pass
 
 func can_see_player():
-	var player_pos = player.get_global_position()
-	var ray_pos = playerCast.get_global_position()
-	var ray_vector = player_pos - ray_pos
-	playerCast.set_cast_to(ray_vector)
-	var coll = playerCast.get_collider()
-	
+	if player != null:
+		var player_pos = player.get_global_position()
+		var ray_pos = playerCast.get_global_position()
+		var ray_vector = player_pos - ray_pos
+		playerCast.set_cast_to(ray_vector)
+		var coll = playerCast.get_collider()
+		if playerCast.is_colliding() and coll.is_in_group("player"):
+			return true
+		else:
+			return false
 
 
 func set_state(new_state: int):
 	if new_state == current_state:
 		return
+	elif new_state == State.PATROL:
+		sprite.set_frame(0)
 	if new_state == State.AIM:
 		animationPlayer.play("summon")
 	current_state = new_state
@@ -75,22 +79,12 @@ func fire():
 
 
 
-func _on_PlayerDetectionZone_body_entered(body):
-	if body.is_in_group("player"):
-		player = body
-		var player_pos = player.get_global_position() * 2
-		player_pos = to_local(player_pos)
-		playerCast.set_cast_to(player_pos) 
-		playerCast.force_raycast_update()
-		var cast_location = playerCast.get_cast_to()
-		
-		var coll = playerCast.get_collider()
-		if playerCast.is_colliding() and coll.is_in_group("player"):
-			set_state(State.AIM)
+
 
 
 func _on_ShotTimer_timeout():
 	if shots_fired < shots_to_fire:
 		fire()
 	else:
+		shots_fired = 0
 		set_state(State.PATROL)
