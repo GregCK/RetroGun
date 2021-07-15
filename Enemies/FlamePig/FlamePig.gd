d0f870e6164ff0d6c7e1bec9b856772fbf6c7516
GregCK <3bluejay@gmail.com>
2021-07-15 16:51:44 -0400

    rotating pig

--- Enemies/FlamePig/FlamePig.gd
+++ Enemies/FlamePig/FlamePig.gd
@@ -26,11 +26,21 @@ func _physics_process(delta):
 			if can_see_player():
 				set_state(State.ALIGN)
 		State.ALIGN:
-			pass
+			align_state(delta)
 		State.CHARGE:
 			pass
 
-
+func align_state(delta):
+	var player_dir_vector = playerCast.get_cast_to()
+	var player_dir_radians = player_dir_vector.angle()
+	var player_dir_degrees = rad2deg(player_dir_radians)
+	
+	
+#	rotation_degrees = lerp(rotation_degrees, player_dir_degrees, 0.1)
+	var old_rotation_radians = deg2rad(rotation_degrees)
+	var new_rotation_radians = lerp_angle(old_rotation_radians, player_dir_radians, 0.1)
+	
+	rotation_degrees = rad2deg(new_rotation_radians)
 
 func set_state(new_state: int):
 	if new_state == current_state:
@@ -48,3 +58,12 @@ func set_state(new_state: int):
 	
 	current_state = new_state
 	emit_signal("state_changed", current_state)
+
+
+static func lerp_angle(from, to, weight):
+	return from + _short_angle_dist(from, to) * weight
+
+static func _short_angle_dist(from, to):
+	var max_angle = PI * 2
+	var difference = fmod(to - from, max_angle)
+	return fmod(2 * difference, max_angle) - difference
---
+++ Enemies/FlamePig/PlayerCast.gd
@@ -0,0 +1,5 @@
+extends RayCast2D
+
+
+func _physics_process(delta):
+	global_rotation_degrees = 0
